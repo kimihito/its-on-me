ab2b5efd6c09c0f3cae4241ea9182b567266d7c0
kimihito <tatsurotamashiro@gmail.com>
2013-08-01 00:35:09 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItsOnMe::Application.config.secret_key_base = 'eade67f57854c699d6f74cb25575787aa67445cf49b96fa82154a4351d2c5e30baef2fd0ab8f1b986c9829dd9602021db8d9de65b9d4ce6a8dc8d1d140887b0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItsOnMe::Application.config.session_store :cookie_store, key: '_its-on-me_session'
